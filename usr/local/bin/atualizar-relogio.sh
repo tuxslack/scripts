f3e092e02fe3da90fc134cfa2ebfb55a4f2d6651
Fernando <33039864+tuxslack@users.noreply.github.com>
2023-10-04 01:01:31 -0300

    Add files via upload

---
+++ usr/local/bin/atualizar-relogio.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# Por Fernando Souza - https://www.youtube.com/@fernandosuporte
+# Data: 02/10/2023 as 12:29
+
+# Atualizar a data e hora do sistema
+
+
+clear
+
+# ----------------------------------------------------------------------------------------
+
+# Verifica se quem executa é o Root
+
+
+if [ "$(id -u)" != "0" ]; then
+
+echo -e "\nVocê deve executar este script como Root! \n"
+
+exit 
+
+
+fi
+
+# ----------------------------------------------------------------------------------------
+
+# Verificar se os programas estão instalados
+
+
+which ping               2> /dev/null || { echo "Programa ping não esta instalado."      ; exit ; }
+which hwclock            2> /dev/null || { echo "Programa hwclock não esta instalado."   ; exit ; }
+which ntpdate            2> /dev/null || { echo "Programa ntpdate não esta instalado."   ; exit ; }
+
+# ----------------------------------------------------------------------------------------
+
+# Verificar o acesso à internet
+
+
+verifica_internet(){
+
+    ping -c 2 www.google.com.br 2>/dev/null
+
+    if [ $? != 0 ] ; then
+    
+    echo "
+Não foi possível atualizar o relógio...
+
+Esta sem acesso à internet..."
+     
+    sleep 30
+        
+    exit 
+    
+    fi
+
+}
+
+
+verifica_internet
+
+    
+# ----------------------------------------------------------------------------------------
+
+    
+clear 
+
+echo "
+Data e hora atual do sistema:
+"
+
+hwclock
+
+
+echo "
+Atualizando a data e hora do sistema...
+"
+ntpdate -u pool.ntp.org
+
+    if [ $? != 0 ] ; then
+    
+
+# notify-send -i gtk-dialog-info  -t 200000 "Atualização de data e hora do sistema..." "
+#
+# Ocorreu um problema em atualizar a data e hora do sistema em: `date +%d/%m/%Y-%H:%M`
+#
+# "
+
+
+echo "
+Ocorreu um problema em atualizar a data e hora do sistema em: `date +%d/%m/%Y-%H:%M`
+"
+
+    sleep 30
+    
+    exit 
+       
+        
+    fi
+    
+
+hwclock -w
+
+
+echo "
+Data e hora atual do sistema:
+"
+
+hwclock
+
+
+
+sleep 30
+
+# ----------------------------------------------------------------------------------------
+
+
+
+exit 0
+    
