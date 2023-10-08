68f0a43d3c19e0410f1c7ace4c13cbafe6d01eb4
Fernando <33039864+tuxslack@users.noreply.github.com>
2023-10-07 23:38:29 -0300

    Add files via upload

--- usr/local/bin/baixar-thumbnail-youtube.sh
+++ usr/local/bin/baixar-thumbnail-youtube.sh
@@ -33,6 +33,11 @@
 # http://smokey01.com/yad/
 # https://www.kabipan.com/densan/yad.html
 # https://askubuntu.com/questions/894888/bash-template-to-use-zenity-or-yad-to-insert-edit-delete-records-in-a-file
+# https://ubuntuforum-br.org/index.php?topic=105582.0
+# http://ubuntued.info/multimedia-no-ubuntu-13-04-raring-ringtail-mp3-flash-java
+# https://docs.xubuntu.org/1710/user/pt_BR/xubuntu-documentation-A4.pdf
+
+
 
 
 
@@ -79,24 +84,6 @@ which convert    2> /dev/null || { yad --center --image=dialog-error --timeout=1
 
 # ----------------------------------------------------------------------------------------
 
-# Os aplicativos KDE não abrem ou visualizam imagens .webp no Void Linux
-#
-# Pacotes ausentes: kimageformats e qt5-imageformats 
-#
-# $ xbps-query -Rs webp
-# [-] imlib2-webp-1.12.1_1       WebP loader for Imlib2 (transitional dummy package)
-# [*] libsharpyuv-1.3.2_1        Library to expose libwebp's sharpyuv functionality
-# [-] libsharpyuv-devel-1.3.2_1  Library to expose libwebp's sharpyuv functionality - development files
-# [*] libwebp-1.3.2_1            WebP image format
-# [-] libwebp-devel-1.3.2_1      WebP image format - development files
-# [*] libwebp-tools-1.3.2_1      WebP image format - tools
-# [-] urlwatch-2.28_1            Tool for monitoring webpages for updates
-# [*] webp-pixbuf-loader-0.2.4_1 WebP Image format loader for GdkPixbuf
-#
-
-
-# Suporte WebP
-
 
 # Listando pacotes instalados
 
@@ -110,8 +97,66 @@ if [ -n "$(which xbps-query 2> /dev/null)" ]; then
 
 # Para Void Linux
 
+
+# Suporte WebP
+
+
+# Os aplicativos KDE não abrem ou visualizam imagens .webp no Void Linux
+#
+# Pacotes ausentes: kimageformats e qt5-imageformats 
+#
+# $ xbps-query -Rs webp
+# [-] imlib2-webp-1.12.1_1       WebP loader for Imlib2 (transitional dummy package)
+# [*] libsharpyuv-1.3.2_1        Library to expose libwebp's sharpyuv functionality
+# [-] libsharpyuv-devel-1.3.2_1  Library to expose libwebp's sharpyuv functionality - development files
+# [*] libwebp-1.3.2_1            WebP image format
+# [-] libwebp-devel-1.3.2_1      WebP image format - development files
+# [*] libwebp-tools-1.3.2_1      WebP image format - tools
+# [-] urlwatch-2.28_1            Tool for monitoring webpages for updates
+# [*] webp-pixbuf-loader-0.2.4_1 WebP Image format loader for GdkPixbuf
+# 
+
+
 xbps-query -l | grep -i webp-pixbuf-loader || exit
 
+
+
+
+# Pré-visualização de arquivos no Thunar (Void Linux)
+#
+#  xbps-query -Rs thumb
+# [*] ffmpegthumbnailer-2.2.2_1          Lightweight video thumbnailer
+# [-] ffmpegthumbnailer-devel-2.2.2_1    Lightweight video thumbnailer - development files
+# [-] ffmpegthumbs-22.12.1_1             FFmpeg-based thumbnail creator for video files
+# [-] gnome-epub-thumbnailer-1.6_2       Thumbnailer for EPub and MOBI books
+# [*] gthumb-3.12.3_1                    Image viewer and browser for the GNOME Desktop
+# [-] gthumb-devel-3.12.3_1              Image viewer and browser for the GNOME Desktop - development files
+# [-] kdegraphics-mobipocket-23.08.0_1   KDE Plasma 5 Thumbnailers for various graphics file formats (transitional dummy package)
+# [-] kdegraphics-thumbnailers-23.08.0_1 KDE Plasma 5 Thumbnailers for various graphics file formats
+# [*] tumbler-4.18.1_1                   D-Bus Thumbnailer service
+# [-] tumbler-devel-4.18.1_1             D-Bus Thumbnailer service - development files
+# [-] tumbler-plugins-extra-4.18.1_1     D-Bus Thumbnailer service - extra plugins (may use network)
+
+
+
+# Pré-visualização de arquivos de PDF e fotos no Thunar
+
+xbps-query -l | grep -i tumbler
+
+
+# Pré-visualização de arquivos de vídeos no Thunar
+
+xbps-query -l | grep -i ffmpegthumbnailer
+
+
+
+# xbps-query -l | grep -i gstreamer
+# ii gst-libav-1.22.5_1                                          GStreamer libav plugin (1.x)
+# ii gst-plugins-bad1-1.22.5_1                                   GStreamer plugins from the bad set (v1.x)
+# ii gst-plugins-base1-1.22.5_1                                  GStreamer Base Plug-ins (v1.x)
+# ii gst-plugins-good1-1.22.5_2                                  GStreamer set of well-maintained good plug-ins (1.x)
+# ii gst-plugins-ugly1-1.22.5_1                                  GStreamer plugins from the ugly set (1.x)
+# ii gstreamer1-1.22.5_1                                         Core GStreamer libraries and elements (1.x)
 
 
 
@@ -252,8 +297,8 @@ link_do_video_do_youtube=$(yad \
 --center \
 --entry \
 --window-icon "/usr/share/icons/extras/youtube.png" \
---title="yt-dlp - baixar áudio de vídeo" \
---text="Qual o link do vídeo que você deseja baixar o áudio?" \
+--title="Baixar thumbnail de vídeo do YouTube - yt-dlp" \
+--text="Qual o link do vídeo que você deseja baixar a capa (thumbnail)?" \
 --entry-text="" \
 --width=700 --entry \
 2> /dev/null)
@@ -286,7 +331,9 @@ fi
 
 # Você precisa copiar o link do vídeo do youtube que você quer baixar a thumbnail. 
 
-yt-dlp --skip-download --write-thumbnail "$link_do_video_do_youtube" 2>> "$log"
+yt-dlp \
+--skip-download \
+--write-thumbnail "$link_do_video_do_youtube" 2>> "$log"
 
 
 # Esse comando vai baixar apenas a thumbnail do vídeo, sem baixar o vídeo inteiro. O 
